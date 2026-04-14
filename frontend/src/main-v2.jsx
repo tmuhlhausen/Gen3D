2571e32c08ed8814056bacaa31a8614afdb5fe23
tmuhlhausen <58753672+tmuhlhausen@users.noreply.github.com>
2026-04-13 17:34:38 -0700

    Add frontend v2 entrypoint

---
+++ frontend/src/main-v2.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import StudioApp from "./StudioApp.jsx";
+import "./studio.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <StudioApp />
+  </React.StrictMode>
+);
